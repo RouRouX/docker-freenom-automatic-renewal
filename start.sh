d3b532e6023c3e0fc262e3c1d1875dc72554aa10
肉肉 <9323294@qq.com>
2020-11-25 01:49:45 +0800

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#删除旧配置
+sed -i '/freenom/d' /var/spool/cron/crontabs/root
+#增加配置
 echo "$CRON cd /freenom/ && php run > freenom_crontab.log 2>&1">>/var/spool/cron/crontabs/root
 # 如果两个配置文件不存在，从备份中复制一波到数据卷中
 if [ ! -f /conf/config.php ]; then
